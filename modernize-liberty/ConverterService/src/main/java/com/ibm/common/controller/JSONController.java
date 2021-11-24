c1c3693975474ed667ab230e2b68439f9bdb168a
Remi Cauchon <REMI.CAUCHON@ibm.com>
2021-11-24 10:15:48 -0500

    Fixing issue with localhost and docker

--- modernize-liberty/ConverterService/src/main/java/com/ibm/common/controller/JSONController.java
+++ modernize-liberty/ConverterService/src/main/java/com/ibm/common/controller/JSONController.java
@@ -26,13 +26,15 @@ public class JSONController {
 
 			Context ctx = new InitialContext();
 			
-			String hostname = System.getenv("server_hostname");
-            System.out.println("Server Hostname: "+ hostname);
-			String port = System.getenv("server_iiop_port");
-			System.out.println("Server iiop port: "+ port);
+			String hostname = System.getenv("ejb_server_hostname");
+            System.out.println("EJB Server Hostname: "+ hostname);
+			String port = System.getenv("ejb_server_iiop_port");
+			System.out.println("EJB Server iiop port: "+ port);
 
-			System.out.println("Server ejb remote: "+ System.getenv("server_ejb_remote_bean"));
-			System.out.println("Server ejb remote: "+ System.getenv("server_ejb_remote"));
+			String ejbRemotePath = System.getenv("ejb_server_remote_path");
+			System.out.println("EJB Server remote path: "+ ejbRemotePath);
+			String ejbRemoteBean = System.getenv("ejb_server_remote_bean");
+			System.out.println("EJB Server remote bean: "+ ejbRemoteBean);
 
 			if (hostname == null || hostname.isEmpty()){
 				hostname = "localhost";
@@ -40,9 +42,12 @@ public class JSONController {
 			if (port == null || port.isEmpty()){
 				port = "22809";
 			}
+
             String provider = "corbaname::" + hostname + ":" + port;
-			                    
-			String ejbGlobalStr = "ejb/global/TempEAR-0.0.1/com.ibm.temp-TempEJB-0.0.1/ConverterBean!com.ibm.temp.ejb.ConverterRemote";
+			                
+			// ejb_server_remote_path=TempEAR-0.0.1/com.ibm.temp-TempEJB-0.0.1/ConverterBean
+            // ejb_server_remote_bean=com.ibm.temp.ejb.ConverterRemote
+			String ejbGlobalStr = "ejb/global/" + ejbRemotePath + "!" + ejbRemoteBean;
 
 			Object homeObject = ctx.lookup(provider + "#" + ejbGlobalStr);
 			ConverterRemote myRemoteEJB = (ConverterRemote) PortableRemoteObject.narrow(homeObject, ConverterRemote.class);
