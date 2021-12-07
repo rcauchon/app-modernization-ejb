3d13f171513c213ecc985db88085af805a899f31
Remi Cauchon <REMI.CAUCHON@ibm.com>
2021-12-07 08:46:59 -0500

    Add the code for the working with full apiUrl

--- modernize-liberty/temp-conv-ui/src/environments/environment.prod.ts
+++ modernize-liberty/temp-conv-ui/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://client-ejb.converter-ejb.svc:9085/ConverterService/rest/converter/'
+ // Not working apiUrl: 'http://client-ejb.converter-ejb.svc.cluster.local:9085/ConverterService/rest/converter/'
+ // Not working apiUrl: 'http://client-ejb.converter-ejb.svc:9085/ConverterService/rest/converter/'
+  apiUrl: 'http://client-ejb-route-converter-ejb.itzroks-6630025ezu-fcstbe-6ccd7f378ae819553d37d5f2ee142bd6-0000.us-south.containers.appdomain.cloud/ConverterService/rest/converter/'
+// Not working  apiUrl: 'http://172.21.85.131:9085/ConverterService/rest/converter/'
 };
